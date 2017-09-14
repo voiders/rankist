b67bb5e3a262b5247ab61f1dc5cbdedc11a72fee
Jose Albizures <albizures3601@gmail.com>
2017-09-13 19:12:14 -0600

    setup nuxt

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,6 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import App from './components/App.vue'
 
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
-
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
